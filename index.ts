24f904b959790393b7366b6e96ec9cdb7f40cee7
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-11-02 01:27:09 -0400

    second

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import * as express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json("reached");
+    res.json({
+        message: "REached"
+    });
 });
 
 app.listen(8080, () => {
